f162113b057e84d567c58b0e55c8723215178e49
NinnaBriones <20126183@tafe.wa.edu.au>
2025-08-14 23:17:25 +0800

    Update scripts_challenge_2.js

--- session-01/assets/js_starter/scripts_challenge_2.js
+++ session-01/assets/js_starter/scripts_challenge_2.js
@@ -4,8 +4,15 @@ let numbers = [1, 2, 3, 4, 5];
 
 // Define the arrow function doubleNumbers
 
+const doubleNumbers = (array) => {
+let double = array.map(number => number * 2);
+return double;
+
+}
 
 // Call the function with the numbers array
+
+let result = doubleNumbers(numbers);
 
 
 // Console log the new array
