de406ba496fe43ef87a7b79eb846876f6a166d60
NinnaBriones <20126183@tafe.wa.edu.au>
2025-08-15 01:07:08 +0800

    Update scripts_challenge_4.js

--- session-02/assets/js_starter/scripts_challenge_4.js
+++ session-02/assets/js_starter/scripts_challenge_4.js
@@ -4,8 +4,24 @@ let numbers = [1, 2, 3, 4, 5, 6];
 
 // Find the index of the number 5 in the array
 
+let five = numbers.findIndex(n => n === 5);
+console.log(five);
+
+
 // If the number 5 is found in the array
 
-// Console log each number in the array up to and including 5
+  // Console log each number in the array up to and including 5
 
-// Output a message if the number 5 is not in the array
+  if (numbers.includes(5)) {
+
+    let oneToFive = numbers.filter(n => n <= 5);
+console.log(oneToFive);
+
+
+  // Output a message if the number 5 is not in the array
+  }
+    else
+  {
+  console.log("Number not in array")
+  }
+
